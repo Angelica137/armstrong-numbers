2327cfac9aa56555da9d20bcaa25b241813a6c98
Angelica <Angelica137@users.noreply.github.com>
2022-08-03 20:26:21 +0100

    refactor

--- scripts/armstrongnumber.py
+++ scripts/armstrongnumber.py
@@ -1,6 +1,3 @@
 def armstrong_number(n: int) -> bool:
     chars = str(n)
-    r = sum([(int(char) ** len(chars)) for char in chars])
-    if r != n:
-        return False
-    return True
+    return n == sum([(int(char) ** len(chars)) for char in chars])
