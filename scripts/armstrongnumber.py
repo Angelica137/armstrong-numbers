1ed8769c97713c2142f5dc2f80335b5d853f0d9f
Angelica <Angelica137@users.noreply.github.com>
2022-08-03 20:14:57 +0100

    add requirements on test red

--- scripts/armstrongnumber.py
+++ scripts/armstrongnumber.py
@@ -1,11 +1,18 @@
 def armstrong_number(n: int) -> bool:
-    n = str(n)
+    num_str = str(n)
     calc = []
-    place = len(n)
-    for char in n:
+    place = len(num_str)
+    for char in num_str:
         calc.append(int(char) ** place)
+        print(calc)
         place -= 1
     result = sum(calc)
+    print(type(result))
+    print(type(n))
     if result != n:
         return False
     return True
+
+
+print(armstrong_number(9))
+print(armstrong_number(10))
